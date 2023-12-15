b525783d6d40e6020bfaa67bfd832e310499ce69
jackzhang2013 <134703887+jackzhang2013@users.noreply.github.com>
2023-12-15 20:30:57 +0800

    Update and rename school.html to index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+function overfunction1()
+{
+    element = document.getElementById('frame1');
+    element.style["box-shadow"] = "0px 15px 10px -15px #DDDDDD";
+}
+
+function outfunction1()
+{
+    element = document.getElementById('frame1');
+    element.style["box-shadow"] = "0px 0px 0px 0px #DDDDDD";
+}
+
+function overfunction2()
+{
+    element = document.getElementById('frame2');
+    element.style["box-shadow"] = "0px 15px 10px -15px #DDDDDD";
+}
+
+function outfunction2()
+{
+    element = document.getElementById('frame2');
+    element.style["box-shadow"] = "0px 0px 0px 0px #DDDDDD";
+}
