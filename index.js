e4c8d55212c5467eb2224935f0abbbeb50a1c9e2
jackzhang2013 <134703887+jackzhang2013@users.noreply.github.com>
2024-04-13 18:43:51 +0800

    重大更新

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-function overfunction1()
+function overfunction(element_name)
 {
-    element = document.getElementById('frame1');
+    element = document.getElementById(element_name);
     element.style["box-shadow"] = "0px 15px 10px -15px #DDDDDD";
 }
 
-function outfunction1()
+function outfunction(element_name)
 {
-    element = document.getElementById('frame1');
+    element = document.getElementById(element_name);
     element.style["box-shadow"] = "0px 0px 0px 0px #DDDDDD";
 }
 
-function overfunction2()
+function clickfunction(name)
 {
-    element = document.getElementById('frame2');
-    element.style["box-shadow"] = "0px 15px 10px -15px #DDDDDD";
+    window.open(name, "_blank");
 }
 
-function outfunction2()
+function clickfunctioninput()
 {
-    element = document.getElementById('frame2');
-    element.style["box-shadow"] = "0px 0px 0px 0px #DDDDDD";
+    val = document.getElementById("input-text");
+    alert("值是" + val.value);
 }
+
+function enterfunction(e)
+{
+    theEvent = e;
+    keyCode = theEvent.keyCode || theEvent.which || theEvent.charCode;
+    if (keyCode == 13) {
+        val = document.getElementById("input-text");
+        alert("值是" + val.value);
+    }
+}
+
+temp = document.getElementById("logo-text");
+temp.onkeydown = enterfunction(e)
